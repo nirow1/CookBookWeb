83891eef102ba20e3b67809d98b4ace5a0e8878d
will <gunmonyk@gmail.com>
2024-01-09 01:55:32 +0100

    mvc migration

---
+++ CookbookLogic/FullRecipesController.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using CookbookDataAccess.DataAccess;
+using CookbookDataAccess.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Web;
+//using System.Web.Mvc;
+//using System.Web.Mvc.Ajax;
+//using Microsoft.AspNetCore.Mvc;
+
+
+namespace CookbookLogic
+{
+
+
+    public class FullRecipesController // : Controller
+    {
+        private readonly RecipeContext RecipeContext;
+
+        public FullRecipesController(RecipeContext recipeContext)
+        {
+            this.RecipeContext = recipeContext;
+        }
+
+        /*public IActionResult Index()
+        {
+            var data = from rec in this.RecipeContext.Recipes
+                       join gui in this.RecipeContext.Guides
+                       on rec.Id equals gui.Id into guideGroup select new
+                       {
+                           rec.Name,
+                           rec.Source,
+                           rec.Score,
+                           rec.Category,
+                           rec.LastCooked,
+                           GuideList = guideGroup.ToList(),
+                       };
+            return (IActionResult)View(data.ToList());
+        }*/
+    }
+}
--- CookbookUI/Program.cs
+++ CookbookUI/Program.cs
@@ -2,14 +2,15 @@ using Microsoft.EntityFrameworkCore;
 using CookbookDataAccess.DataAccess;
 using CookbookLogic;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<RecipeContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
-builder.Services.AddRazorPages();
 builder.Services.AddTransient<FullRecipesController>();
 
 var app = builder.Build();
@@ -17,7 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -29,6 +30,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
