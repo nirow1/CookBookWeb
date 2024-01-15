8857f37b9992f117adbe351990eff02d9b08f4d9
will <gunmonyk@gmail.com>
2024-01-15 20:56:21 +0100

    Running details

--- CookbookUI/Controllers/FullRecipesController.cs
+++ CookbookUI/Controllers/FullRecipesController.cs
@@ -18,9 +18,26 @@ namespace CookbookUI.Controllers
             var data = from rec in context.Recipes
                        join gui in context.Guides
                        on rec.Id equals gui.Id into guideGroup
-                       select new { Name = rec.Name, Score = rec.Score, Category = rec.Category, LastCooked = rec.LastCooked,  GuideList =guideGroup.ToList()};
+                       select new {Id = rec.Id, Name = rec.Name, Score = rec.Score, Category = rec.Category, LastCooked = rec.LastCooked,  GuideList =guideGroup.ToList()};
 
             return View(data.ToList());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipes = await context.Recipes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recipes == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipes);
         }
     }
 }
