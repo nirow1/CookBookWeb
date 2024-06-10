c3a85f1be471823df2b678bb6679b958caff8e24
will <gunmonyk@gmail.com>
2024-06-10 22:02:40 +0200

    added cookbook dto

---
+++ CookbookLogic/Dto/GuideDto.cs
@@ -0,0 +1,40 @@
+﻿using CookbookDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookbookLogic.Dto
+{
+    public class GuideDto
+    {
+        public GuideDto()
+        {
+              
+        }
+
+        public GuideDto(Guides guides)
+        {
+            Id = guides.Id;
+            RecipeId = guides.RecipeId;
+            Type = guides.Type;
+            Score = guides.Score;
+            WritenGuide = guides.WritenGuide;
+            TotalCalories = guides.TotalCalories;
+            TotalProtein = guides.TotalProtein;
+            TotalGrams = guides.TotalGrams;
+        }
+
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+        public string? Type { get; set; }
+        public float Score { get; set; }
+        public string? WritenGuide { get; set; }
+        public float TotalCalories { get; set; }
+        public float TotalProtein { get; set; }
+        public float TotalGrams { get; set; }
+    }
+}
---
+++ CookbookLogic/Dto/RecipeDto.cs
@@ -0,0 +1,31 @@
+﻿using CookbookDataAccess.Models;
+
+namespace CookbookLogic.Dto
+{
+    public class RecipeDto
+    {
+        public RecipeDto()
+        {
+            
+        }
+
+        public RecipeDto(Recipes recipe)
+        {
+            Id = recipe.Id;
+            Name = recipe.Name;
+            Category = recipe.Category;
+            Source = recipe.Source;
+            Score = recipe.Score;
+            LastCooked = recipe.LastCooked;
+            Guides = recipe.Guides.Select(g => new GuideDto(g)).ToList();
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string? Source { get; set; }
+        public float Score { get; set; }
+        public DateTime LastCooked { get; set; }
+        public List<GuideDto> Guides { get; set; } = new List<GuideDto>();
+    }
+}
